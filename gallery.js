eca3a5852c85abb0cd582da6fcf4fd778541d25f
Bayakz <159006935+Josephkwicha004@users.noreply.github.com>
2024-07-27 14:01:22 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 // JavaScript for image navigation
 let currentImageIndex = 0;
-const images = ['p1.jpg', 'p2.jpg', 'p7.jpg','p5.jpg', 'p4.jpg','p8.jpg']; // Add paths to your images here
+const images = ['p1.jpg', 'p10.jpg', 'p7.jpg','p5.jpg', 'p4.jpg','p8.jpg']; // Add paths to your images here
 
 function showImage(index) {
   const imageElement = document.getElementById('gallery-image');
